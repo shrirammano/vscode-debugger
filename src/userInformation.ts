efcc3806591273cfc637efb277fcda6220c2d726
shriram <shriram.m@crystaldelta.com>
2021-10-08 11:41:19 +1100

    Change date field

--- src/userInformation.ts
+++ src/userInformation.ts
@@ -1,7 +1,7 @@
 export const getUserInformation = (userId: string) => {
   const users = [
-    { userName: "John",  userId: "1", certifiedDate: "1/10/2021"},
-    { userName: "James", userId: "2", certifiedDate: "10/10/2021"}
+    { userName: "John",  userId: "1", acknowledgementDate: "1/10/2021"},
+    { userName: "James", userId: "2", acknowledgementDate: "10/10/2021"}
   ];
   return users.find(user => user.userId === userId);
 }
